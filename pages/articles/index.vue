3c477d5a69ec98bd24da40fb0717bca92b9de840
Tskon <tsekhmeister.k@gmail.com>
2018-12-15 17:29:00 +0300

    add content

---
+++ pages/articles/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    Статьи
+    <li>test1</li>
+    <li>test2</li>
+    <li>test3</li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: "index"
+  }
+</script>
+
+<style scoped>
+
+</style>
