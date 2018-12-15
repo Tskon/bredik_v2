d4f3d3891c681a7c4227ce555cea3f7729ee6709
Tskon <tsekhmeister.k@gmail.com>
2018-12-15 17:06:48 +0300

    add header to main template

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,12 @@
 <template>
-  
+  <header>
+    <logo></logo>
+    <nav>
+      <a href="/">Главная</a>
+      <a href="#">Блог</a>
+      <a href="#">Обо мне</a>
+    </nav>
+  </header>
 </template>
 
 <script>
