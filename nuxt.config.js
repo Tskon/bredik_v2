d413745fbdf4d12c1e69a9aa3d70d702ff46ba5a
Tskon <tsekhmeister.k@gmail.com>
2018-12-16 13:07:05 +0300

    add global styles and meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,24 @@
-module.exports = {
+const path = require('path');
 
-}
+module.exports = {
+  head: {
+    titleTemplate: title => (title) ? `${title} - Bredik v2` : 'Bredik v2',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    ],
+    htmlAttrs: {
+      lang: 'ru',
+      amp: undefined // "amp" has no value
+    }
+  },
+  modules: [
+    [
+      'nuxt-sass-resources-loader', [
+      'assets/scss/variables.scss',
+      'assets/scss/global.scss'
+    ]
+    ]
+  ],
+
+};
