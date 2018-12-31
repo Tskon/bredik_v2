88a8593a24799b283b044b23f6ef48fdb3d6e5b8
tskon <tsekhmeister.k@gmail.com>
2018-12-31 21:01:41 +0300

    add about page

---
+++ pages/about-me/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <h1>Обо мне</h1>
+    <section>
+      <h2>Frontend developer</h2>
+      <p>Пишу на React, Vue. Активно интересуюсь SSR для Vue.</p>
+      <p>Люблю современный стек технологий. Использую в своих проектах es6+, Sass, Babel, Webpack</p>
+    </section>
+  </main>
+</template>
+
+<script>
+  export default {
+    name: "index"
+  }
+</script>
+
+<style scoped>
+
+</style>
