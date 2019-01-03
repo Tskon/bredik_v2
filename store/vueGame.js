4669807c41faf5a106fd7ca783a7e1425a5b7aff
tskon <tsekhmeister.k@gmail.com>
2019-01-03 15:28:08 +0300

    add data

--- store/vueGame.js
+++ store/vueGame.js
@@ -1,5 +1,30 @@
 export const state = () => ({
-  list: [5,3,7]
+  // карта координат. Хранит диапазоны координат и колбеки
+  xyMap: {
+
+  },
+
+  hero: {
+    immortal: false,
+    size:{
+      width: 50,
+      height: 50,
+    },
+    position: {
+      x: 0,
+      y: 0,
+    },
+    ammo:{
+      m4a1: false,
+      shotgun: false
+    },
+    parameters:{
+      hp: 100,
+      armor: 50,
+      speed: 10,
+    }
+  }
+
 });
 
 export const mutations = {
