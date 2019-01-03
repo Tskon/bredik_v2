4ba3239251da5e25d79ba963cb31126f64546151
tskon <tsekhmeister.k@gmail.com>
2019-01-03 12:45:32 +0300

    add path for vue-game

---
+++ pages/projects/vue-game.vue
@@ -0,0 +1,18 @@
+<template>
+  <vue-game></vue-game>
+</template>
+
+<script>
+  import vueGame from 'components/vue-game/index'
+
+  export default {
+    name: "vue-game",
+    components:{
+      vueGame
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
