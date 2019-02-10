fb98f59bb665a781c8e02110854141e2a4bc7e88
Tskon <tsekhmeister.k@gmail.com>
2019-02-10 14:58:30 +0300

    add bullet

---
+++ components/vue-game/bullet.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="bullet"></div>
+</template>
+
+<script>
+  export default {
+    name: "bullet",
+    data(){
+      return {
+
+      }
+    },
+    props:{
+
+    },
+    methods:{
+
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
